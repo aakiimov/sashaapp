f48bf30a53aaaa809bb0164f4f32b5cb3dbf3d5e
aakiimov <dragantsevv@ya.ru>
2020-11-27 20:04:37 +0300

    Updates

---
+++ precache-manifest.59375e45cc5f33ffab31b083304dd5e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3567d46b5d20e4fca9b",
+    "url": "/static/css/main.f2a471ef.chunk.css"
+  },
+  {
+    "revision": "f3567d46b5d20e4fca9b",
+    "url": "/static/js/main.02e6c460.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01f6bc71da5d0529d404",
+    "url": "/static/css/2.e59b596f.chunk.css"
+  },
+  {
+    "revision": "01f6bc71da5d0529d404",
+    "url": "/static/js/2.6e670635.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "eb9ffbfa86aba34fd8865241f015ff5a",
+    "url": "/index.html"
+  }
+];
